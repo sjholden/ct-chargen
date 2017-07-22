45b433f037ae3545a1245e4ebeed3b53b2009c0b
Sam Holden <sholden@holden.id.au>
2017-07-22 16:24:19 -0400

    Add test for career enlistment roll.

--- test/test_character.py
+++ test/test_character.py
@@ -23,3 +23,44 @@ class Test(unittest.TestCase):
             char = character.Character(fixRolls=[1,1,1,1,1,1,1,1,1,1,1,1,1,1,d,6,6])
             char.selectCareer(character.NAVY)
             self.assertEqual(char.career, c)
+            
+    def testEnlistment(self):
+        """Test the enlistment rolls."""
+        def nonOtherTest(career, enlistment, dm1stat, dm1num, dm2stat, dm2num):
+            for dm1roll in self._getAll2DSums():
+                dm1sum = sum(dm1roll)
+                for dm2roll in self._getAll2DSums():
+                    dm2sum = sum(dm2roll)
+                    for enlistroll in self._getAll2DSums():
+                        enlistsum = sum(enlistroll)
+                        if dm1sum >= dm1num:
+                            enlistsum += 1
+                        if dm2sum >= dm2num:
+                            enlistsum += 2
+                        statrolls = [1,1,1,1,1,1,1,1,1,1,1,1]
+                        statrolls[dm1stat*2:dm1stat*2+2] = dm1roll
+                        statrolls[dm2stat*2:dm2stat*2+2] = dm2roll
+                        char = character.Character(fixRolls=statrolls + enlistroll + [6] )
+                        char.selectCareer(career)
+                        if enlistsum >= enlistment:
+                            self.assertEqual(char.career, career)
+                        else:
+                            self.assertEqual(char.career, character.OTHER)
+        nonOtherTest(character.NAVY, 8, character.INT, 8, character.EDU, 9)
+        nonOtherTest(character.MARINES, 9, character.INT, 8, character.STR, 8)
+        nonOtherTest(character.ARMY, 5, character.DEX, 6, character.END, 5)
+        nonOtherTest(character.SCOUTS, 7, character.INT, 6, character.STR, 8)
+        nonOtherTest(character.MERCHANTS, 7, character.STR, 7, character.INT, 6)
+        for enlistroll in self._getAll2DSums():
+            char = character.Character(fixRolls=[1,1,1,1,1,1,1,1,1,1,1,1]+enlistroll+[1])
+            char.selectCareer(character.OTHER)
+            if sum(enlistroll) >= 3:
+                self.assertEqual(char.career, character.OTHER)
+            else:
+                self.assertEqual(char.career, character.NAVY)
+                
+                        
+                    
+        
+    def _getAll2DSums(self):
+        return [[1,1], [1,2], [1,3], [1,4], [1,5], [1,6], [2,6], [3,6], [4,6], [5,6], [6,6]]
